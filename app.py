a195f8a99ffa2e190f4ea4e0a343c87168b8d033
githansa <swarnahansa@hotmail.com>
2023-11-14 14:36:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 print("hello")
 my_lis =['red','blue','white']
 print(my_lis[0])
+
+print("hello")
+my_lis =['red','blue','white']
+print(my_lis[0])
+
+print("hello")
+my_lis =['red','blue','white']
+print(my_lis[0])
