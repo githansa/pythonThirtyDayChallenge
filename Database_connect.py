8698e749402ce57ced1dd16fa9d2d2a513dd0e98
githansa <swarnahansa@hotmail.com>
2023-12-01 15:03:29 -0600

    day_five added

---
+++ Database_connect.py
@@ -0,0 +1,6 @@
+#connect to database using python
+
+import cx_Oracle as cx
+import pandas as pd
+import time
+
--- Day_five.py
+++ Day_five.py
@@ -1,1 +1,28 @@
-print('Hello Day Five List and Dictionaries')
+print('Conditionals and Control Flow')
+200 > 201
+
+not (10 > 20)
+
+'abc'.title()
+
+abc = [2, 4, 6, 7, 9, 13]
+abc_sq = []
+for num in abc:
+    new_number = num ** 2
+    abc_sq.append(new_number)
+
+# check to see if the array is odd or even
+print(abc_sq)
+i = 0
+for num in abc_sq:
+    if (num % 2) == 0:
+        print('Even')
+        i = i + 1
+    else:
+        print('Odd')
+        i = i + 1
+
+# put in is_odd and is_even array
+
+
+# else if is elif in Python
